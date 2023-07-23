52fbfb599a2f8d987255cbf31d7af248815a821b
basilisk <d0ix9oqh@anonaddy.me>
2023-07-23 15:04:41 +0100

    Rewrite CacheManager to make use of CacheIndex

--- yaht/cache_management.py
+++ yaht/cache_management.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import pickle
 import sqlite3
 
 
@@ -95,21 +96,35 @@ class CacheIndex:
 class CacheManager:
     def __init__(self, cache_dir):
         """Connect to the cache, initialize it if necessary"""
-        self.cache_dir = cache_dir
-        if not os.path.exists(self.cache_dir):
-            os.makedirs(self.cache_dir)
-
-        # Create or connect to the cache index
-        self.reload_index()
-
-        self.data = {}
-
-    def reload_index(self):
-        """Connect to the index that stores cache descriptors etc"""
-        pass
+        self.cache_index = CacheIndex(cache_dir)
 
     def send_data(self, key, data):
-        self.data[key] = data
+        """Save the data and record its metadata in the cache index"""
+        # Create a new item if it doesn't exist
+        if not self.cache_index.check_item_exists(key):
+            self.cache_index.add_item(key, data["legend"])
+        # Write the data to the relevant filename
+        filename = self.cache_index.get_item_filename(key)
+        with open(filename, "wb") as f:
+            pickle.dump(data["data"], f)
 
     def get_data(self, key):
-        return self.data[key]
+        """Load the data from the cache by the given key"""
+        filename = self.cache_index.get_item_filename(key)
+        with open(filename, "rb") as f:
+            raw_data = pickle.load(f)
+        data_legend = self.cache_index.get_item_legend(key)
+        data = {"data": raw_data, "legend": data_legend}
+        return data
+
+    def check_data(self, key):
+        return self.cache_index.check_item_exists(key)
+
+    def delete_data(self, key):
+        """Delete the data file and relevant cache index entry"""
+        if not self.cache_index.check_item_exists(key):
+            return
+
+        filename = self.cache_index.get_item_filename(key)
+        os.remove(filename)
+        self.cache_index.delete_item(key)
