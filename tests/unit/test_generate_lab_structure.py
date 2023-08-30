7c7167ca856062a328227f336270a33c735157fc
professional-username <9hacahn5@anonaddy.me>
2023-08-30 23:06:39 +0100

    Create generate_laboratory_structure method

---
+++ tests/unit/test_generate_lab_structure.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+import pytest
+import pandas as pd
+from yaht.laboratory import generate_laboratory_structure
+
+
+@pytest.fixture
+def mock_all_procs(mocker):
+    def mock_get_process(proc_name):
+        return lambda x: "%s_%s" % (x, proc_name)
+
+    mocker.patch("yaht.trial.get_process", mock_get_process)
+
+
+def test_simple_data_input(mock_all_procs):
+    """Test the most bare essential config that reads input from a given hash"""
+    config = {
+        "experiments": {
+            "exp1": {
+                "inputs": ["hash:DATA_HASH"],
+                "structure": {
+                    "foo": ["inputs.0"],
+                },
+                "outputs": ["foo"],
+            }
+        },
+    }
+
+    # Generate the structure
+    structure = generate_laboratory_structure(config)
+    structure.index = structure["name"]  # For easier testing
+
+    # There should be a single row for a single process
+    assert type(structure) == pd.DataFrame
+    assert len(structure) == 1
+    # The process should have the correct assigned experiment
+    assert structure.loc["foo", "experiment"] == "exp1"
+    # The input hash of the single process should be correct
+    assert structure.loc["foo", "dep_hashes"] == ["DATA_HASH"]
+
+
+def test_simple_file_input(mock_all_procs):
+    """Test the most bare essential config that reads input from a file"""
+    config = {
+        "input_files": {"EXAMPLE_FILE": "FILE_HASH"},
+        "experiments": {
+            "exp1": {
+                "inputs": ["file:EXAMPLE_FILE"],
+                "structure": {
+                    "foo": ["inputs.0"],
+                },
+                "outputs": ["foo"],
+            }
+        },
+    }
+
+    # Generate the structure
+    structure = generate_laboratory_structure(config)
+    structure.index = structure["name"]  # For easier testing
+
+    # The input hash of the single process should be correct
+    assert structure.loc["foo", "dep_hashes"] == ["FILE_HASH"]
+
+
+def test_multiple_experiments(mock_all_procs):
+    """Test setting up multiple experiments"""
+    config = {
+        "experiments": {
+            "exp1": {
+                "inputs": ["hash:DATA_HASH"],
+                "structure": {
+                    "foo": ["inputs.0"],
+                },
+                "outputs": ["foo"],
+            },
+            "exp2": {
+                "inputs": ["hash:ANOTHER_DATA_HASH"],
+                "structure": {
+                    "foo": ["inputs.0"],
+                    "bar": ["foo"],
+                },
+                "outputs": ["bar"],
+            },
+        },
+    }
+
+    # Generate the structure
+    structure = generate_laboratory_structure(config)
+    structure.set_index(["experiment", "name"], inplace=True)  # For easier testing
+
+    # There should be three processes
+    assert type(structure) == pd.DataFrame
+    assert len(structure) == 3
+    # Check that every process has the correct dependency hashes
+    assert structure.loc[("exp1", "foo"), "dep_hashes"] == ["DATA_HASH"]
+    assert structure.loc[("exp2", "foo"), "dep_hashes"] == ["ANOTHER_DATA_HASH"]
+    assert structure.loc[("exp2", "bar"), "dep_hashes"] == [
+        structure.loc[("exp2", "foo"), "hash"]
+    ]
--- yaht/laboratory.py
+++ yaht/laboratory.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import pandas as pd
 from yaht.cache_management import CacheManager
-from yaht.experiment import Experiment
+from yaht.experiment import Experiment, generate_experiment_structure
 
 
 class Laboratory:
@@ -19,15 +19,6 @@ class Laboratory:
         for exp_name in self.experiments:
             self.experiments[exp_name].run_trials()
 
-    def export_experiment_results(self):
-        """Get the results of every experiment and pass them to the experiment"""
-        results = pd.DataFrame(columns=["data", "experiment", "trial", "process"])
-        for exp_name in self.experiments:
-            exp_results = self.experiments[exp_name].get_outputs()
-            exp_results["experiment"] = exp_name
-            results = pd.concat([results, exp_results], ignore_index=True)
-        self.data_exporter.export_data(results)
-
     def set_data(self, key, data, metadata):
         self.cache_manager.send_data(key, data, metadata)
 
@@ -43,3 +34,52 @@ class Laboratory:
         key = self.cache_manager.get_keys_by_metadata(fname, "filename")[0]
         data = self.cache_manager.get_data(key)
         return data
+
+    def export_experiment_results(self):
+        """Get the results of every experiment and pass them to the experiment"""
+        results = pd.DataFrame(columns=["data", "experiment", "trial", "process"])
+        for exp_name in self.experiments:
+            exp_results = self.experiments[exp_name].get_outputs()
+            exp_results["experiment"] = exp_name
+            results = pd.concat([results, exp_results], ignore_index=True)
+        self.data_exporter.export_data(results)
+
+    # def export_output_metadata(self, data_exporter):
+    # """Get the metadata for each piece of output data and export it"""
+    # output_keys = []
+    # for exp_name in self.experiments:
+    #     output_keys.append(self.experiments[exp_name])
+    # metadata =
+
+
+def generate_laboratory_structure(config):
+    """Convert a nested dictionary config into a dataframe structure"""
+    laboratory_structure = pd.DataFrame()
+
+    # Generate every experiment structure
+    input_files = config["input_files"] if "input_files" in config else {}
+    experiment_configs = config["experiments"]
+    for exp_name, exp_config in experiment_configs.items():
+        # Substitute in the input hashes for each experiment TODO: Extract to seperate method?
+        exp_inputs = exp_config["inputs"]
+        input_hashes = []
+        for exp_input in exp_inputs:
+            input_type, input_name = exp_input.split(":")
+            if input_type == "file":
+                input_hash = input_files[input_name]
+            elif input_type == "hash":
+                input_hash = input_name
+            else:
+                raise ValueError("Invalid input type '%s'" % input_type)
+            input_hashes.append(input_hash)
+        # Generate the experimennt structure
+        exp_config["inputs"] = input_hashes
+        exp_structure = generate_experiment_structure(exp_config)
+        # Set other values for the experiment
+        exp_structure["experiment"] = exp_name
+        # Append to the global lab structure
+        laboratory_structure = pd.concat(
+            [laboratory_structure, exp_structure], ignore_index=True
+        )
+
+    return laboratory_structure
