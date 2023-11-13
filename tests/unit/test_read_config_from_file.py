2bc40e35ab440b66b310c34641250e5eb6fd0263
professional-username <9hacahn5@anonaddy.me>
2023-11-13 17:35:07 +0000

    Add yaml config reading and processing functions

---
+++ tests/unit/test_read_config_from_file.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python3
+import os
+import pytest
+import shutil
+import tempfile
+from yaht.config_processing import read_config_file
+
+
+def gen_config_file(config):
+    """Setup a temp directory to run the tests in"""
+    temp_dir = tempfile.mkdtemp()
+    config_fname = os.path.join(temp_dir, "config.yaml")
+    with open(config_fname, "w") as f:
+        f.write(config)
+    return config_fname
+
+
+def clean_dict(d):
+    """
+    Recursively remove all keys with values that are None, empty collections etc. from a dictionary.
+    For easier comparison between generated and expected configs
+    """
+    if not isinstance(d, (dict, list)):
+        return d
+    if isinstance(d, list):
+        return [v for v in (clean_dict(v) for v in d) if v]
+    return {k: v for k, v in ((k, clean_dict(v)) for k, v in d.items()) if v}
+
+
+def test_process_sources():
+    """Test the loading of the structure and process sources"""
+    yaml_config = "\n".join(
+        [  # Global sources are set as a dictionary
+            "SOURCES:",
+            '  some_file: "file:example_file"',
+            '  another_file: "file:example_file"',
+            "some_experiment:",
+            "  results: foo",
+            "",
+            # The sources for each process are set as comma-separated strings
+            "  structure:",
+            "    foo: some_file",
+            "    bar: some_file, another_file",
+        ]
+    )
+    config_fname = gen_config_file(yaml_config)
+
+    config = clean_dict(read_config_file(config_fname))
+    expected_config = {
+        "sources": {
+            "some_file": "file:example_file",
+            "another_file": "file:example_file",
+        },
+        "experiments": {
+            "some_experiment": {
+                # Each process should be expanded into a dictionary
+                "structure": {
+                    "foo": {
+                        "sources": ["some_file"],
+                        "function": "foo",
+                    },
+                    "bar": {
+                        "sources": ["some_file", "another_file"],
+                        "function": "bar",
+                    },
+                },
+                "results": ["foo"],
+            }
+        },
+    }
+    assert config == expected_config
+
+
+def test_multiple_trials():
+    """test reading a config that specifies multiple trials"""
+    yaml_config = "\n".join(
+        [
+            "SOURCES:",
+            '  some_file: "file:example_file"',
+            "some_experiment:",
+            "  results: foo",
+            "",
+            "  structure:",
+            "    foo: some_file",
+            "",
+            # Each trial is specified as a dictionary of parameters
+            "  trials:",
+            "    trial1:",
+            "      p1: 3",
+            "    trial2:",
+            "      p1: -1",
+        ]
+    )
+    config_fname = gen_config_file(yaml_config)
+
+    config = clean_dict(read_config_file(config_fname))
+    expected_config = {
+        "sources": {"some_file": "file:example_file"},
+        "experiments": {
+            "some_experiment": {
+                "structure": {
+                    "foo": {
+                        "sources": ["some_file"],
+                        "function": "foo",
+                    }
+                },
+                "results": ["foo"],
+                # The experiment will be carried out with the same structure
+                # for each set of parameters specified in the trials
+                "trials": {
+                    "trial1": {"p1": 3},
+                    "trial2": {"p1": -1},
+                },
+            }
+        },
+    }
+    assert config == expected_config
+
+
+def test_parameters():
+    """test reading a config that specifies global parameters"""
+    yaml_config = "\n".join(
+        [
+            "SOURCES:",
+            '  some_file: "file:example_file"',
+            "some_experiment:",
+            "  results: foo",
+            "",
+            "  structure:",
+            "    foo: some_file",
+            "",
+            # The global parameters are also specified as a dictionary of parameters
+            "  parameters:",
+            "    p1: 12",
+        ]
+    )
+    config_fname = gen_config_file(yaml_config)
+
+    config = clean_dict(read_config_file(config_fname))
+    expected_config = {
+        "sources": {"some_file": "file:example_file"},
+        "experiments": {
+            "some_experiment": {
+                "structure": {
+                    "foo": {
+                        "sources": ["some_file"],
+                        "function": "foo",
+                    }
+                },
+                "results": ["foo"],
+                # These are global parameters
+                # that apply to all trials in an experiment
+                "parameters": {"p1": 12},
+            }
+        },
+    }
+    assert config == expected_config
+
+
+def test_setting_process_results():
+    """Test naming or specifying multiple results of a process"""
+    yaml_config = "\n".join(
+        [
+            "SOURCES:",
+            '  some_file: "file:example_file"',
+            "some_experiment:",
+            "  results: foo",
+            "",
+            # An arrow after the sources of a proc specifies a list of result names
+            "  structure:",
+            "    foo: some_file -> foo_result",
+            "    bar: some_file -> bar_one, bar_two",
+        ]
+    )
+    config_fname = gen_config_file(yaml_config)
+
+    config = clean_dict(read_config_file(config_fname))
+    expected_config = {
+        "sources": {"some_file": "file:example_file"},
+        "experiments": {
+            "some_experiment": {
+                # The results of each process should now be specified
+                "structure": {
+                    "foo": {
+                        "sources": ["some_file"],
+                        "results": ["foo_result"],
+                        "function": "foo",
+                    },
+                    "bar": {
+                        "sources": ["some_file"],
+                        "results": ["bar_one", "bar_two"],
+                        "function": "bar",
+                    },
+                },
+                "results": ["foo"],
+            }
+        },
+    }
+    assert config == expected_config
+
+
+def test_specifying_global_results():
+    """Test specifying the global results of the experiment"""
+    yaml_config = "\n".join(
+        [
+            "SOURCES:",
+            '  some_file: "file:example_file"',
+            "some_experiment:",
+            # Global results are set as a string seperated by commas
+            "  results: foo, bar_two",
+            "",
+            "  structure:",
+            "    foo: some_file",
+            "    bar: some_file -> bar_one, bar_two",
+        ]
+    )
+    config_fname = gen_config_file(yaml_config)
+
+    config = clean_dict(read_config_file(config_fname))
+    expected_config = {
+        "sources": {"some_file": "file:example_file"},
+        "experiments": {
+            "some_experiment": {
+                "structure": {
+                    "foo": {
+                        "sources": ["some_file"],
+                        "function": "foo",
+                    },
+                    "bar": {
+                        "sources": ["some_file"],
+                        "results": ["bar_one", "bar_two"],
+                        "function": "bar",
+                    },
+                },
+                # The global results of the experiment should be a list of two
+                "results": ["foo", "bar_two"],
+            }
+        },
+    }
+    assert config == expected_config
+
+
+def test_setting_process_functions():
+    """Test overriding the function that a process represents"""
+    yaml_config = "\n".join(
+        [
+            "SOURCES:",
+            '  some_file: "file:example_file"',
+            "some_experiment:",
+            "  results: foo",
+            "",
+            "  structure:",
+            # A proc's function is overriden with an arrow in the dict key
+            "    foo <- bar: some_file",
+        ]
+    )
+    config_fname = gen_config_file(yaml_config)
+
+    config = clean_dict(read_config_file(config_fname))
+    expected_config = {
+        "sources": {"some_file": "file:example_file"},
+        "experiments": {
+            "some_experiment": {
+                # The function of the process should now be specified
+                "structure": {
+                    "foo": {
+                        "sources": ["some_file"],
+                        "function": "bar",
+                    },
+                },
+                "results": ["foo"],
+            }
+        },
+    }
+    assert config == expected_config
---
+++ yaht/config_processing.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+import yaml
+
+
+def read_config_file(config_fname):
+    """Read a yaml config file into a nested dictionary structure"""
+
+    # First simply load the file into a dictionary
+    with open(config_fname, "r") as config_stream:
+        raw_config = yaml.safe_load(config_stream)
+
+    # Then process every item in the config
+    config = {
+        "experiments": {},
+    }
+    for key, value in raw_config.items():
+        # Top level-sections come in different types
+        match key:
+            case "SOURCES":
+                config["sources"] = value
+            # If they don't fall into one of the above categories,
+            # they represent an experiment
+            case _:
+                config["experiments"][key] = process_experiment_config(value)
+
+    return config
+
+
+def process_experiment_config(raw_experiment_config):
+    """Process the user-readable experiment config into program-readable"""
+    experiment_config = {}
+
+    # Structure
+    raw_structure_config = raw_experiment_config["structure"]
+    experiment_config["structure"] = process_structure_config(raw_structure_config)
+
+    # Results
+    results_string = raw_experiment_config.get("results", "")
+    results_list = results_string.split(",")
+    results_list = list(map(str.strip, results_list))
+    experiment_config["results"] = results_list
+
+    # Trials
+    raw_trials_config = raw_experiment_config.get("trials", {})
+    experiment_config["trials"] = raw_trials_config
+
+    # Parameters
+    raw_parameters_config = raw_experiment_config.get("parameters", {})
+    experiment_config["parameters"] = raw_parameters_config
+
+    return experiment_config
+
+
+def process_structure_config(raw_structure_config):
+    """
+    Turn one-line process key-value pairs into dicts
+    specifying the parameters of the process
+    """
+    structure_config = {}
+    for key, value in raw_structure_config.items():
+        # First generate the process name and function from the key
+        proc_name = key.split("<-")[0]
+        proc_function = key.split("<-")[1] if "<-" in key else proc_name
+
+        # Then generate the sources and results from the value
+        sources_string = value.split("->")[0].strip()
+        sources_list = sources_string.split(",")
+        results_string = value.split("->")[1] if "->" in value else ""
+        results_list = results_string.split(",")
+
+        # Strip all parameters of extraneous whitespace
+        proc_name = proc_name.strip()
+        proc_function = proc_function.strip()
+        sources_list = list(map(str.strip, sources_list))
+        results_list = list(map(str.strip, results_list))
+
+        # Combine into a single process config and add to structure config
+        structure_config[proc_name] = {
+            "function": proc_function,
+            "sources": sources_list,
+            "results": results_list,
+        }
+    return structure_config
