d8aae6d85ea9b7e7542f862b8a6e598c1b4aa4e9
basilisk <d0ix9oqh@anonaddy.me>
2023-12-26 20:33:33 +0000

    Rewrite Laboratory to use new cache and structure

--- tests/unit/test_laboratory.py
+++ tests/unit/test_laboratory.py
@@ -1,115 +1,71 @@
 #!/usr/bin/env python3
 import os
+import shutil
 import pytest
 import tempfile
 import pandas as pd
-from yaht.cache_management import CacheManager
+import yaht.cache_management as CM
 from yaht.laboratory import Laboratory
 
 
-class MockDataExporter:
-    """A mock data exporter that records any data 'exported'"""
-
-    def __init__(self):
-        self.data = []
-
-    def export_data(self, data):
-        self.data.append(data)
-
-
-# Setup a temp directory to run the tests in
 @pytest.fixture
-def cache_dir():
-    new_dir = tempfile.mkdtemp()
-    yield os.path.join(new_dir, "cache")
-    # shutil.rmtree(new_dir)
-
-
-# Override the get_process method
-@pytest.fixture
-def mock_processes(mocker):
+def mock_all_procs(mocker):
     def mock_get_process(proc_name):
         return lambda x: "%s_%s" % (x, proc_name)
 
-    mocker.patch("yaht.trial.get_process", mock_get_process)
+    mocker.patch("yaht.structure.get_process", mock_get_process)
 
 
-# A base lab config to run and tinker with
 @pytest.fixture
-def base_lab_config(cache_dir):
+def mock_config():
+    # Create a mock cache
+    new_dir = tempfile.mkdtemp()
+    cache_dir = os.path.join(new_dir, "cache")
+    CM.store_cache_data(cache_dir, "example_key", "EXAMPLE_DATA")
+    source_fname = (
+        CM.load_cache_metadata(cache_dir)
+        .set_index("hash")
+        .loc["example_key", "filename"]
+    )
+
     config = {
-        "cache_dir": cache_dir,
+        "settings": {
+            "cache_dir": cache_dir,
+        },
+        "sources": {
+            "some_data": "file:" + source_fname,
+        },
         "experiments": {
-            "exp1": {
-                "inputs": ["INPUT"],
-                "outputs": ["p1"],
-                "structure": {"p1": ["inputs.0"]},
+            "some_experiment": {
+                "structure": {
+                    "foo": {
+                        "sources": ["some_data"],
+                        "function": "foo",
+                    },
+                    "bar": {
+                        "sources": ["foo"],
+                        "function": "bar",
+                        "results": ["bar_result"],
+                    },
+                },
+                "results": ["bar_result"],
             }
         },
     }
     yield config
+    shutil.rmtree(new_dir)
 
 
-def test_base_config(mock_processes, base_lab_config):
-    """Test that running a simple config exports the correct data"""
-    data_exporter = MockDataExporter()
-    lab = Laboratory(base_lab_config, data_exporter)
-
+def test_lab_run_experiments(mock_config, mock_all_procs):
+    """We should be able to run the above experiment and return the results"""
+    lab = Laboratory(mock_config)
     lab.run_experiments()
-    lab.export_experiment_results()
-
-    # Output should be a dataframe
-    outputs = data_exporter.data[0]
-
-    expected_outputs = pd.DataFrame(columns=["data", "experiment", "trial", "process"])
-    expected_outputs["experiment"] = ["exp1"]
-    expected_outputs["trial"] = ["control"]
-    expected_outputs["process"] = ["p1"]
-    expected_outputs["data"] = ["INPUT_p1"]
-
-    assert outputs.equals(expected_outputs)
-
-
-def test_reading_input_data_from_file(mock_processes, base_lab_config, cache_dir):
-    """
-    Test that we can write data to a file in the cache
-    and use it as input to the experiment
-    """
-    # Write some data to the cache
-    external_cache_manager = CacheManager(cache_dir)
-    external_cache_manager.send_data(
-        key="inputFile",
-        data="FILE_INPUT_DATA",
-        metadata={"filename": "inputFile"},
-    )
-
-    # Update the config to use the file as input
-    updated_config = base_lab_config
-    updated_config["experiments"]["exp1"]["inputs"] = ["file:inputFile"]
-
-    # Run the lab with the new config
-    data_exporter = MockDataExporter()
-    lab = Laboratory(updated_config, data_exporter)
-    lab.run_experiments()
-    lab.export_experiment_results()
-    outputs = data_exporter.data[0]
-
-    assert outputs["data"].values == ["FILE_INPUT_DATA_p1"]
-
-
-def test_multiple_experiments():
-    # Necessary?
-    pass
-
-
-def test_export_outputs_metadata():
-    """Test exporting the metadata for the laboratory's outputs"""
-
-
-def test_export_input_metadata():
-    # ??
-    pass
-
-
-def test_export_all_metadata():
-    pass
+    results = lab.get_results()
+    # The results should be a dataframe of data
+    assert type(results) == pd.DataFrame
+    assert len(results) == 1
+    assert results["experiment"][0] == "some_experiment"
+    assert results["trial"][0] == "control"
+    assert results["process"][0] == "bar"
+    assert results["name"][0] == "bar_result"
+    assert results["value"][0] == "EXAMPLE_DATA_foo_bar"
