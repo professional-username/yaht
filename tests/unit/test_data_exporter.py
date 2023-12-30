8827a5e7058515899bb2f5cf4be636d786d60880
professional-username <9hacahn5@anonaddy.me>
2023-12-30 17:06:22 +0000

    Remove outdated functions

--- tests/unit/test_data_exporter.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from yaht.data_exporter import DataExporter
-
-
-def test_pass_data_to_base_exporter():
-    """Test passing data to the base exporter class"""
-    exporter = DataExporter()
-    example_data = "example data"
-    exporter.export_data(example_data)
