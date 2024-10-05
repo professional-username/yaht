4d0507d6d220bd20dd816a86afcce8b3aeba1797
professional-username <9hacahn5@anonaddy.me>
2024-10-05 23:54:44 +0100

    Create devenv environment

---
+++ devenv.nix
@@ -0,0 +1,35 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  env.GREET = "yaht: yet another hyperparameter tuner";
+
+  packages = [ pkgs.git ];
+
+  languages.python = {
+    enable = true;
+    poetry = {
+      enable = true;
+      install = {
+        enable = true;
+        installRootPackage = false;
+        onlyInstallRootPackage = false;
+        compile = false;
+        quiet = false;
+        groups = [ ];
+        ignoredGroups = [ ];
+        onlyGroups = [ ];
+        extras = [ ];
+        allExtras = false;
+        verbosity = "no";
+      };
+      activate.enable = true;
+      package = pkgs.poetry;
+    };
+  };
+
+  scripts.project-test.exec = ''
+    pytest
+  '';
+
+  # See full reference at https://devenv.sh/reference/options/
+}
